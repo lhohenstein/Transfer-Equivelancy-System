518827ad864d51284522e2453f3185aaec10b68d
lhohenstein <33071674+lhohenstein@users.noreply.github.com>
2018-04-02 10:49:51 -0400

    Add files via upload

---
+++ display.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title> Table with Database>
+   </head>
+   <body>
+   <table> 
+     <tr>
+       <th>Transfer_School_Code</th>
+       <th>Transfer_School_Name</th>
+       <th>Transfer_School_Course_Code</th>
+       <th>Transfer_Course_Name</th>
+       <th>Arcadia_Course_Code</th>
+       <th>Arcadia_Course_Name</th>
+       <th>General_Education</th>
+       <th>Minimum_Grade</th>
+       <th>core_to_core</th>
+     </tr>
+
+    <table>
+     	<?php>
+     	$conn = mysqli_connect("localhost", "root","", "csv_db");
+     	if ($conn-> connect_error){
+     	  die("Connection failed:". $conn-> connect_error);
+     }
+     $sql = "SELECT * table_name";
+     $result = $conn-> query($sql);
+
+     if ($result-> num_rows > 0){
+       while ($row = $result-> fetch_assoc()){
+          echo "<tr><td>". $row["Transfer_School_Code"] ."</td><td>".  $row["Transfer_School_Name"]."</td><td>".  $row["Transfer_School_Course_Code"]. "</td><td>".  $row["Transfer_Course_Name"]. "</td><td>".  $row["Arcadia_Course_Code"]. "</td><td>".  $row["Arcadia_Course_Name"]. "</td><td>".  $row["General_Education"]. "</td><td>".  $row["Minimum_Undergraduate_Transfer_Grade"]. "</td><td>".  $row["Core_To_Core_Agreement"].
+   }
+   echo "</table>";
+ }
+ else{
+ 	echo "0 result";
+        }
+        $conn-> close();
+        ?>
+</table>
+</body>
+</html>
