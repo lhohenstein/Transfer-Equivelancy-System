061af986401de1e17d6eab947feb5e3579f1204f
krtrey <33071672+krtrey@users.noreply.github.com>
2018-04-25 12:28:11 -0400

    Add files via upload

---
+++ ProcessFinal.php
@@ -0,0 +1,131 @@
+<?php 
+  $db = mysqli_connect('localhost', 'root', '', 'schooldata');
+  $schoolname = "";
+  $coursecode = "";
+  $grade="";
+  $AuCc="";
+  $ARgrade="";
+  $genEd="";
+  $schoolname1 = "";
+  $coursecode1 = "";
+  $grade1 = "";
+  $AuCc1 = "";
+  $ARgrade1="";
+  $genEd1="";
+  $creditTotal=0;
+  if (isset($_POST['RegisterFinal'])) {
+  	$schoolname = $_POST['schoolname'];
+  	$coursecode = $_POST['coursecode'];
+  	$credit = $_POST['credits'];
+    $grade= $_POST['grade'];
+  	$sql_u = "SELECT TRANSFER_SCHOOL_NAME FROM transferdata WHERE TRANSFER_SCHOOL_NAME='$schoolname'";
+  	$sql_e = "SELECT TRANSFER_SCHOOL_COURSE_CODE FROM transferdata WHERE  TRANSFER_SCHOOL_COURSE_CODE = '$coursecode'" ;
+	$sql_x = "SELECT ARCADIA_COURSE_CODE FROM transferdata WHERE TRANSFER_SCHOOL_COURSE_CODE = '$coursecode'";
+  	$res_u = mysqli_query($db, $sql_u);
+  	$res_e = mysqli_query($db, $sql_e);
+	$res_x = mysqli_query($db, $sql_x);
+    while ( $row=mysqli_fetch_row($res_x))
+    {
+    $AuCc =$row[0];
+  }
+    $sql_g = "SELECT MINIMUM_UNDERGRADUATE_TRANSFER_GRADE FROM audata WHERE  ARCADIA_COURSE_CODE = '$AuCc'";
+    $sql_y = "SELECT GENERAL_EDUCATION FROM audata WHERE ARCADIA_COURSE_CODE = '$AuCc'";
+    $res_g = mysqli_query($db, $sql_g);
+    $res_y = mysqli_query($db, $sql_y);
+while ( $row=mysqli_fetch_row($res_g))
+    {
+    $ARgrade =$row[0];
+  }
+  while ( $row=mysqli_fetch_row($res_y))
+    {
+    $genEd =$row[0];
+  }
+//echo strcmp($grade, $ARgrade) ;
+  	if (mysqli_num_rows($res_u) <1) {
+  	  $school_error = 'School is not in database - Please Contact the Registrar Office';
+      echo "$school_error"; 	
+  	}
+  	else if(mysqli_num_rows($res_e) <1){
+  	  $code_error = 'course is not in database - Please contact the Registrar Office';
+      }
+     else if (strcmp($grade , $ARgrade) == '<1')
+      {
+        $grade_error = "You do not have a high enough grade  '$ARgrade'";
+        echo "Grade is not accepted.";
+      }
+      else{
+           $creditTotal+=$credit;
+           //echo " You will receive $credit credits";
+           echo "________________ CLASS 1 ________________";
+           echo "<br>";
+           echo " Arcadia course code: $AuCc";
+            echo "<br>";
+            echo "<br>";
+           echo " Requirements Fulfilled: $genEd"; 
+           echo "<br>";
+           echo "<br>";
+           echo " Credit total = $creditTotal";	
+           echo "<br>";
+           echo "<br>";
+         }
+         echo "<br>";
+    $schoolname1 = $_POST['schoolname1'];
+  	$coursecode1 = $_POST['coursecode1'];
+  	$credit1 = $_POST['credits1'];
+    $grade1= $_POST['grade1'];
+  	$sql_u1 = "SELECT TRANSFER_SCHOOL_NAME FROM transferdata WHERE TRANSFER_SCHOOL_NAME='$schoolname1'";
+  	$sql_e1 = "SELECT TRANSFER_SCHOOL_COURSE_CODE FROM transferdata WHERE  TRANSFER_SCHOOL_COURSE_CODE = '$coursecode1'" ;
+	$sql_x1 = "SELECT ARCADIA_COURSE_CODE FROM transferdata WHERE TRANSFER_SCHOOL_COURSE_CODE = '$coursecode1'";
+  	$res_u1 = mysqli_query($db, $sql_u1);
+  	$res_e1 = mysqli_query($db, $sql_e1);
+	$res_x1 = mysqli_query($db, $sql_x1);
+    while ( $row1=mysqli_fetch_row($res_x1))
+    {
+    $AuCc1 =$row1[0];
+  }
+    $sql_g1 = "SELECT MINIMUM_UNDERGRADUATE_TRANSFER_GRADE FROM audata WHERE  ARCADIA_COURSE_CODE = '$AuCc1'";
+    $sql_y1 = "SELECT GENERAL_EDUCATION FROM audata WHERE ARCADIA_COURSE_CODE = '$AuCc1'";
+    $res_g1 = mysqli_query($db, $sql_g1);
+    $res_y1 = mysqli_query($db, $sql_y1);
+while ( $row1=mysqli_fetch_row($res_g1))
+    {
+    $ARgrade1 =$row1[0];
+  }
+  while ( $row1=mysqli_fetch_row($res_y1))
+    {
+    $genEd1 =$row1[0];
+  }
+// echo strcmp($grade1, $ARgrade1) ;
+  	if (mysqli_num_rows($res_u1) <1) {
+  	  $school_error1 = 'School is not in database - Please Contact the Registrar Office';
+      echo "School is not in database - Please Contact the Registrar Office";    	
+  	}
+  	else if(mysqli_num_rows($res_e1) <1){
+  	  $code_error1 = 'course is not in database - Please contact the Registrar Office';
+      echo "course is not in database - Please contact the Registrar Office";
+      }
+     else if (strcmp($grade1 , $ARgrade1) == 1)
+      {
+        $grade_error1 = "YOu do not have a high enough grade  '$ARgrade1'";
+        echo "Grade is not accepted.";
+      }
+      else{
+           $creditTotal+=$credit1;
+           //echo " You will receive $credit1 credits";
+           echo "________________ CLASS 2 ________________";
+           echo "<br>";
+           echo "<br>";
+           echo " Arcadia course code: $AuCc1";
+           echo "<br>";
+           echo "<br>";
+           echo " Requirements Fulfilled: $genEd1"; 
+            echo "<br>";
+           echo "<br>";	
+           echo " Credit total = $creditTotal";
+         }
+         echo "<br>";
+         echo "<br>";
+         echo "***If a course equivalency does not display on this page please ensure that the school is printed as listed on the school list. Otherwise the school and/or course information is not currently in the database and you should contact the registrar at registrar@arcadia.edu***";
+         exit();
+      
+      }
---
+++ RegisterFinal.php
@@ -0,0 +1,77 @@
+<?php include('ProcessFinal.php') ?>
+<html>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" media="screen" href="System_Layout.css">
+</head>
+
+   <body>
+  
+      <h1> Transfer Equivalency Form</h1>
+<div align="center">
+</body>
+
+<body>
+  <form method="post" action="RegisterFinal.php" id="register_form">
+    <h3>Input your transfer course information below to view Arcadia course equivalency and general education requirements</h3>
+    <h2>Course 1</h2>
+    <div <?php if (isset($school_error)): ?> class="form_error" <?php endif ?> >
+    <input type="text" name="schoolname" placeholder="SCHOOL NAME" value="<?php echo $schoolname; ?>">
+    <?php if (isset($school_error)): ?>
+      <span><?php echo $school_error; ?></span>
+    <?php endif ?>
+    </div>
+    <div <?php if (isset($code_error)): ?> class="form_error" <?php endif ?> >
+      <input type="text" name="coursecode" placeholder="COURSE CODE" value="<?php echo $coursecode; ?>">
+      <?php if (isset($code_error)): ?>
+        <span><?php echo $code_error; ?></span>
+      <?php endif ?>
+    </div>
+    <div>
+      <input type="number"  placeholder="CREDITS" name="credits">
+    </div>
+  <div <?php if (isset($grade_error)): ?> class="form_error" <?php endif ?> >
+    <input type="text" name="grade" placeholder="EARNED GRADE" value="<?php echo $grade; ?>">
+     <?php if (isset($grade_error)): ?>
+        <span><?php echo $grade_error; ?></span>
+      <?php endif ?>
+    </div>
+    
+
+    <h2>Course 2</h2>
+    <div <?php if (isset($school_error1)): ?> class="form_error" <?php endif ?> >
+    <input type="text" name="schoolname1" placeholder="SCHOOL NAME" value="<?php echo $schoolname1; ?>">
+    <?php if (isset($school_error1)): ?>
+      <span><?php echo $school_error1; ?></span>
+    <?php endif ?>
+    </div>
+    <div <?php if (isset($code_error1)): ?> class="form_error" <?php endif ?> >
+      <input type="text" name="coursecode1" placeholder="COURSE CODE" value="<?php echo $coursecode1; ?>">
+      <?php if (isset($code_error1)): ?>
+        <span><?php echo $code_error1; ?></span>
+      <?php endif ?>
+    </div>
+    <div>
+      <input type="number"  placeholder="CREDITS" name="credits1">
+    </div>
+  <div <?php if (isset($grade_error1)): ?> class="form_error" <?php endif ?> >
+    <input type="text" name="grade1" placeholder="EARNED GRADE" value="<?php echo $grade1; ?>">
+     <?php if (isset($grade_error1)): ?>
+        <span><?php echo $grade_error1; ?></span>
+      <?php endif ?>
+    </div>
+    <div>
+      <button type="submit" name="RegisterFinal" id="reg_btn">SUBMIT COURSES</button>
+      <button type="reset" name="reset" id="reg_btn1">RESET</button>
+    </div>
+  </form>
+  </body>
+  <footer>
+  <p>For any additional questions, or to report missing course information, please contact the Registrar's office <br>
+    Taylor Hall, Room 103 <br>
+    215-572-2100 <br>
+    215-572-2126 (fax) <br>
+    registrar@arcadia.edu
+</footer>
+
+</html>
